9b41d7945330c0f310a1352588b14e36f3b647d0
vassmate <vassmate08@gmail.com>
2015-11-04 12:28:00 +0100

    Név bekérés kicsit formázva, gender javítva

--- Donor.py
+++ Donor.py
@@ -141,26 +141,30 @@ class DonorData(object):
 
         return mobile_number
 
+    @staticmethod
     def i_need_your_name():
-        global Name  # A fügévnyen kívül is lehet használni ezt a változót
+        global name     # A fügévnyen kívül is lehet használni ezt a változót
         isvalid = False
         while not isvalid:
-            Name = input("Enter your full name: ")
+            name = input("Enter your full name: ")
 
-            split_Name = Name.split(" ")  # szétszedi a bekért adatot név részekre
+            split_name = name.split(" ")        # Szétszedi a bekért adatot név részekre
 
             try:
-                if Name.replace(" ", "").isalpha() and len(split_Name) > 1:  # A név legalább 2 részből áll és csak betűket tartlmazhat
-                    isvalid = True
+                isvalid = name.replace(" ", "").isalpha()\
+                          and len(split_name) > 1       # A név legalább 2 részből áll és csak betűket tartlmazhat
 
-                else:  # Ha pl számokat adott meg  akkor ezt ki írja és újra lekéri.
-                    print("Try Again, it is not a name. The name can contain only letters and space! ex.: Angela Smith")
-            except:
-                pass
+                if not isvalid:
+                    raise ValueError
 
-        return Name
+            except ValueError:      # Ha pl számokat adott meg  akkor ezt ki írja és újra lekéri.
+                print("Try Again, it is not a name. The name can contain only letters and space! ex.: Angela Smith")
 
+        return name
+
+    @staticmethod
     def get_gender():
+        global gender
         entered_data_is_valid = False
         available_genders = ["f", "m"]
         while not entered_data_is_valid:
