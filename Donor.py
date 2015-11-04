f8a07e2b522f1aefb5f5ba1bb7ab62d6bbef6620
Bandita69 <aaandris96@gmail.com>
2015-11-04 11:59:13 +0100

    Name Added

--- Donor.py
+++ Donor.py
@@ -17,6 +17,7 @@ Blood_types = ["a", "b", "ab", "0"] # Blood Types kisbetűvel egy listában
 
 
 class DonorData(object):
+
     @staticmethod
     # Weight
     def i_need_your_kg():
@@ -139,6 +140,25 @@ class DonorData(object):
                 print("The given '%s' mobile number is not in a valid format!" % mobile_number)
 
         return mobile_number
+
+    def i_need_your_name():
+        global Name  # A fügévnyen kívül is lehet használni ezt a változót
+        isvalid = False
+        while not isvalid:
+            Name = input("Enter your full name: ")
+
+            split_Name = Name.split(" ")  # szétszedi a bekért adatot név részekre
+    
+            try:
+                if Name.replace(" ", "").isalpha() and len(split_Name) > 1:  # A név legalább 2 részből áll és csak betűket tartlmazhat
+                    isvalid = True
+
+                else:  # Ha pl számokat adott meg  akkor ezt ki írja és újra lekéri.
+                    print("Try Again, it is not a name. The name can contain only letters and space! ex.: Angela Smith")
+            except:
+                pass
+
+        return Name
 
 
 
