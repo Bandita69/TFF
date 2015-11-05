00813d89f2e850c6e36a6ce61d6b7857ee4e6170
atillaahun <szikora.atilla@gmail.com>
2015-11-05 11:03:22 +0100

    event.py city + address def added

--- Event.py
+++ Event.py
@@ -30,16 +30,30 @@ class EventData(object):
         return True
 
     # @Atilla
-    # Definition explanation comes here...
+    # Asks for the donor's city.
     @staticmethod
     def get_city():
-        return True
+        cities = ["Miskolc", "Kazincbarcika", "Szerencs", "Sarospatak"]
+        # Asks for the input here first.
+        city = input("Please enter the donor's city: ")
+        # Keeps asking for the city while it does not match one from the cities list.
+        while city not in cities:
+            city = input("Donor's are accepted only from the following cities:\
+            Miskolc, Kazincbarcika, Szerencs and Sarospatak: ")
+        # Returns with the city.
+        return city
 
     # @Atilla
-    # Definition explanation comes here...
+    # Asks for the donor's address.
     @staticmethod
     def get_address():
-        return True
+        # Asks for the input here first.
+        street = input("Please enter the donor's address: ")
+        # Keeps asking for the address while it does not less or equal than 25 characters.
+        while len(street) <= 25:
+            street = input("The address should be less than 25 characters!: ")
+        # Returns with the address.
+        return street
 
     # @Mate
     # Definition explanation comes here...
